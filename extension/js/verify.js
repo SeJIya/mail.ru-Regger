540633d51ee7036effe0b75973f026d54bbc0250
Dmitry Selyanin <39203346+SeJIya@users.noreply.github.com>
2019-03-26 16:53:53 +0500

    Update verify.js

--- extension/js/verify.js
+++ extension/js/verify.js
@@ -1,26 +1,23 @@
 $(document).ready(function() {
 	chrome.storage.local.get('login', function(result){
 		let mail = result.login;
-		let usermail = $('#PH_user-email').text();
-		if(mail == usermail){
-			chrome.storage.local.get('password', function(result){
-				let password = result.password;
-				$.ajax({
-					type:'POST',
-					url:'http://example.com/api/',
-					data:{'key':'1337', type:'mail', action: 'set', login: mail, password},
-					response:'json',
-					success: function (json) {
-						console.log(json);
-						let sucsess = json['sucsess'];
-						if(sucsess){
-							window.open("https://account.mail.ru/signup/simple#auto","_self");
-						}else{
-							console.warn('Error: Ошибка записи данных!');
-						}
+		chrome.storage.local.get('password', function(result){
+			let password = result.password;
+			$.ajax({
+				type:'POST',
+				url:'http://example.com/api/',
+				data:{'key':'1337', type:'mail', action: 'set', login: mail, password},
+				response:'json',
+				success: function (json) {
+					console.log(json);
+					let sucsess = json['sucsess'];
+					if(sucsess){
+						window.open("https://account.mail.ru/signup/simple#auto","_self");
+					}else{
+						console.warn('Error: Ошибка записи данных!');
 					}
-				});
-			});	
-		}
+				}
+			});
+		});	
 	});	
 });
